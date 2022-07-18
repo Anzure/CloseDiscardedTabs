ff0791356e65122b80e0671ccf2dd5adcc299641
André Mathisen <9752808+anzure@users.noreply.github.com>
2022-07-18 18:44:50 +0200

    🎉 Possibility to restore old tabs.

--- background.js
+++ background.js
@@ -1,5 +1,34 @@
+let closedTabs = [];
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
-  if (changeInfo.discarded) {
-    chrome.tabs.remove(tabId);
+  if (!changeInfo.discarded) {
+    return;
   }
+  chrome.tabs.get(tabId, function (tab) {
+    closedTabs.push(tab.url);
+    updateStorage();
+    chrome.tabs.remove(tabId);
+  });
 });
+
+chrome.action.onClicked.addListener(() => {
+  chrome.storage.local.get(["tabs"], (data) => {
+    var oldTabs = JSON.parse(data.tabs);
+    oldTabs.forEach(function (tabUrl) {
+      chrome.tabs.create({
+        url: tabUrl,
+      });
+    });
+  });
+  closedTabs = [];
+  updateStorage();
+});
+
+function updateStorage() {
+  chrome.storage.local.clear(["tabs"]);
+  var saveTabs = JSON.stringify(closedTabs);
+  chrome.storage.local.set({ tabs: saveTabs });
+  chrome.action.setBadgeText({ text: `${closedTabs.length}` });
+}
+
+updateStorage();
